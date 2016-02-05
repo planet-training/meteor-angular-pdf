90be93d2488cb42763b1d4404c305a4b211ad71b
Max Bruchmann <maxbruchmann@gmail.com>
2016-02-05 22:51:38 +0100

    first commit

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'planettraining:angular-pdf',
+  version: '1.3.0',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use('angular:angular@1.4.8')
+  api.addFiles('angularjs-pdf/dist/angular-pdf.min.js','client');
+});
